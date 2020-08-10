ce24001e747cc75df56680e39967272494c8f1e2
m8dotpie <alentev.olm@ya.ru>
2020-08-10 15:53:15 +0300

    solved first task

--- Lab1/task1.cpp
+++ Lab1/task1.cpp
@@ -7,15 +7,15 @@ const float eps = 1e-8;
 
 int main() {
     // 0...N - 1 is x coordinates, N...2N - 1 is y coordinates.
-    float vertexes[6];
+    float coordinates[6];
     for (int i = 0; i < 3; ++i) {
-        scanf("%f %f", &vertexes[i], &vertexes[3 + i]);
+        scanf("%f %f", &coordinates[i], &coordinates[3 + i]);
     }
 
     // Find the area of give polygon.
     float area =
-          (vertexes[0] - vertexes[1]) * (vertexes[3 + 1] - vertexes[3 + 2])
-        - (vertexes[1] - vertexes[2]) * (vertexes[3 + 0] - vertexes[3 + 1]);
+          (coordinates[0] - coordinates[1]) * (coordinates[3 + 1] - coordinates[3 + 2])
+        - (coordinates[1] - coordinates[2]) * (coordinates[3 + 0] - coordinates[3 + 1]);
     area = abs(area) / 2;
 
     // If area is near zero, then polygon does not exist.
